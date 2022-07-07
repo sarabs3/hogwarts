c8df67827d2ebd28e396e95a5749da840d93d5ee
sarab <sarabsingh212@gmail.com>
2022-07-07 15:03:20 +0530

    Attandance component with styling and test

---
+++ src/components/Attendance/Attandance.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import Attandance from './Attendance';
+
+
+describe('Attandance Component', () => {
+    it('should render the component onto the screen', () => {
+        render(<Attandance />);
+        expect(screen.getByText("Attandance")).toBeInTheDocument();
+    });
+})
--- src/components/Attendance/Attendance.tsx
+++ src/components/Attendance/Attendance.tsx
@@ -1,16 +1,18 @@
 import { useContext } from 'react';
 import AvailabilityContext from '../../context/AvailabilityContext';
 import TeacherListItem from './components/TeacherListItem';
+import styles from './attandance.module.css';
 
 const Attandance = () => {
     const { teachers } = useContext(AvailabilityContext);
     return (
-        <div>
-            {teachers.map(teacher => (
-                <div key={teacher.id}>
-                    <TeacherListItem teacher={teacher} />
-                </div>
-            ))}
+        <div className={styles.attandanceContainer}>
+            <h4 className={styles.heading}>Attandance</h4>
+            <div className={styles.attandance}>
+                {teachers.map(teacher => (
+                    <TeacherListItem key={teacher.id} teacher={teacher} />
+                ))}
+            </div>
         </div>
     )
 };
