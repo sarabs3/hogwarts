429b944d41a70695deccd67b82ada9a11dd0163e
sarab <sarabsingh212@gmail.com>
2022-07-07 12:48:21 +0530

    Integrating all the components in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Attandance from './components/Attendance/Attendance';
+import Schedule from './components/Schedule/Schedule';
+import { AvailabilityProvider } from './context/AvailabilityContext';
 
-function App() {
-  return (
+const App = () => (
+  <AvailabilityProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Attandance />
+      <Schedule />
     </div>
-  );
-}
+  </AvailabilityProvider>
+);
 
 export default App;
--- src/components/Schedule/components/StudentListItem.tsx
+++ src/components/Schedule/components/StudentListItem.tsx
@@ -1,15 +1,16 @@
 import React, { useContext } from 'react';
 import AvailabilityContext from '../../../context/AvailabilityContext';
 import { Student } from '../../../types';
+import { getTeacher } from '../../../utils';
 
 type Props = {
     student: Student,
 };
 
 const StudentListItem: React.FC<Props> = ({ student }) => {
-    const {getTeacher} = useContext(AvailabilityContext);
-    
-    const teacher = getTeacher(student.teacherId, student.subject.id);
+    const {teachers} = useContext(AvailabilityContext);
+
+    const teacher = getTeacher(teachers, student.teacherId, student.subject.id);
     return (
         <div key={student.id}>
             {student.name} | {student.subject.name} | {teacher && teacher?.name}
