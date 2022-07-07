8183e0d03eaf7182be7cb4416ee3ce97dc627aa1
sarab <sarabsingh212@gmail.com>
2022-07-07 15:00:26 +0530

    Schedule component styling and test

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,14 +1,26 @@
 import { students } from '../../data/students';
 import StudentListItem from './components/StudentListItem';
+import styles from './Schedule.module.css';
 
 const Schedule = () => (
-  <div>
-    {students.map(student => (
-      <div key={student.id}>
-        <StudentListItem student={student} />
-      </div>
-    )
-    )}
+  <div className={styles.scheduleContainer}>
+    <h4 className={styles.heading}>Today's Schedule</h4>
+    <div className={styles.schedule}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th className={styles.tableHeading}>Student Name</th>
+            <th className={styles.tableHeading}>Subject Name</th>
+            <th className={styles.tableHeading}>Teacher Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map(student => (
+            <StudentListItem key={student.id} student={student} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   </div>
 );
 
---
+++ src/components/Schedule/Sdchedule.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import Schedule from './Schedule';
+
+
+describe('Schedule Component', () => {
+    it('should render the component onto the screen', () => {
+        render(<Schedule />);
+        expect(screen.getByText("Today's Schedule")).toBeInTheDocument();
+    });
+})
